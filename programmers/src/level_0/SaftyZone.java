55e2bae99aca527ac676f6d3b1f4ef003e884f52
JUNHYUNG CHO <junhyungcho@JUNHYUNGui-MacBookPro.local>
2022-12-28 23:41:27 +0900

    safty zone

--- programmers/src/level_0/SaftyZone.java
+++ programmers/src/level_0/SaftyZone.java
@@ -3,38 +3,42 @@ package level_0;
 public class SaftyZone {
 
 	public class Main {
-	    public static void main(String[] args) {
+		public static void main(String[] args) {
 
-	        int[][] board = {{0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 1, 0, 0}, {0, 0, 0, 0, 0}};
+			int[][] param = { { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 1, 0, 0 },
+					{ 0, 0, 0, 0, 0 } };
+			int safeZone = 0;
+			
+			// 위험지역 Set
+			for (int i = 0; i < param.length; i++) {
+				for (int j = 0; j < param.length; j++) {
+					if (param[i][j] == 1)
+						setArea(param, i, j);
+				}
+			}
 
-	        int[][] direct = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 0}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+			// 안전지역 Count
+			for (int i = 0; i < param.length; i++) {
+				for (int j = 0; j < param.length; j++) {
+					if (param[i][j] == 0)
+						safeZone++;
+				}
+			}
 
-	        int length = board.length;
-	        boolean[][] checkBoard = new boolean[length][length];
-	        for (int i = 0; i < length; i++) {
-	            for (int j = 0; j < length; j++) {
-	                if (board[i][j] == 1) {
-	                    for (int[] k : direct) {
-	                        int nearX = i + k[0];
-	                        int nearY = j + k[1];
-	                        if (nearX >= 0 && nearX < length && nearY >= 0 && nearY < length) {
-	                            checkBoard[nearX][nearY] = true;
-	                        }
-	                    }
-	                }
-	            }
-	        }
+			System.out.println(safeZone);
+		}
 
-	        int dangerZoneCount = 0;
-	        for (int i = 0; i < length; i++) {
-	            for (int j = 0; j < length; j++) {
-	                if (checkBoard[i][j]) {
-	                    dangerZoneCount++;
-	                }
-	            }
-	        }
-	        System.out.println(length * length - dangerZoneCount);
-	    }
+		static void setArea(int[][] param, int x, int y) {
+			for (int i = -1; i < 2; i++) {
+				for (int j = -1; j < 2; j++) {
+					try {
+						if (param[x + i][y + j] == 0)
+							param[x + i][y + j] = 2;
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
 	}
-
 }
